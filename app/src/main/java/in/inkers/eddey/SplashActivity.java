48cb5e3f1a1f8a6c37ea595cf13222e9accdc4cb
Zade <mubaraknet123789@gmail.com>
2017-09-13 22:26:51 +0530

    initial commit

---
+++ app/src/main/java/in/inkers/eddey/SplashActivity.java
@@ -0,0 +1,32 @@
+package in.inkers.eddey;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zade on 13/9/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        Thread myThread=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        myThread.start();
+    }
+}
